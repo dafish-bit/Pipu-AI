b06f207358030f3866e112f0136e887b63eb669b
dafish-bit <angelsonico024@gmail.com>
2025-09-10 20:29:09 -0300

    Paso 1 de voz

---
+++ voz.py
@@ -0,0 +1,11 @@
+import speech_recognition as speech_recog
+#### V1 - función
+def speech():
+    mic = speech_recog.Microphone()
+    recog = speech_recog.Recognizer()
+    with mic as audio_file:
+        print("Habla ):<")
+        recog.adjust_for_ambient_noise(audio_file)
+        audio = recog.listen(audio_file)
+        return recog.recognize_google(audio, language="es-GB")
+print(speech())
