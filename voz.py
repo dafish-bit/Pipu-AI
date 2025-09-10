438bc74fadee113162c6426612becfcf439f013a
dafish-bit <angelsonico024@gmail.com>
2025-09-10 20:52:23 -0300

    e

--- voz.py
+++ voz.py
@@ -1,11 +1,14 @@
 import speech_recognition as speech_recog
 #### V1 - función
 def speech():
-    mic = speech_recog.Microphone()
-    recog = speech_recog.Recognizer()
-    with mic as audio_file:
-        print("Habla ):<")
-        recog.adjust_for_ambient_noise(audio_file)
-        audio = recog.listen(audio_file)
-        return recog.recognize_google(audio, language="es-GB")
+    try:
+        mic = speech_recog.Microphone()
+        recog = speech_recog.Recognizer()
+        with mic as audio_file:
+            print("Habla ):<")
+            recog.adjust_for_ambient_noise(audio_file)
+            audio = recog.listen(audio_file)
+            return recog.recognize_google(audio, language="es-GB")
+    except Exception as e:
+        return f"Ni idea ¯\_(ツ)_/¯ {e}"
 print(speech())
